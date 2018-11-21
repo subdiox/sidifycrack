cb3aced93debd9e0ecdba1efd606839ebcade9da
subdiox <subdiox@gmail.com>
2018-11-21 13:51:33 +0900

    Initial Commit

---
+++ sidifycrack/sidifycrack.h
@@ -0,0 +1,13 @@
+//
+//  sidifycrack.h
+//  sidifycrack
+//
+//  Created by subdiox on 2018/11/21.
+//  Copyright © 2018 subdiox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sidifycrack : NSObject
+
+@end
---
+++ sidifycrack/sidifycrack.m
@@ -0,0 +1,13 @@
+//
+//  sidifycrack.m
+//  sidifycrack
+//
+//  Created by subdiox on 2018/11/21.
+//  Copyright © 2018 subdiox. All rights reserved.
+//
+
+#import "sidifycrack.h"
+
+@implementation sidifycrack
+
+@end
